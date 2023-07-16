d37cde38c087425744c0a9545faecf1ca14ef981
i-m-rishabh <uniquepandey125@gmail.com>
2023-07-16 20:50:31 +0530

    added download csv file feature

--- src/pages/home/Premium.js
+++ src/pages/home/Premium.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import {MdDarkMode, MdPictureAsPdf} from 'react-icons/md';
 import {BsFillBrightnessHighFill} from 'react-icons/bs';
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { themeActions } from "../../store/theme";
 import classes from './premium.module.css';
 
@@ -9,7 +9,9 @@ const Premium = () => {
     const [isPremium, setPremium] = useState(false);
     const dispatch = useDispatch();
     const expenseAmount = useSelector(state => state.expense.totalExpenseAmount);
+    const expenses = useSelector(state => state.expense.expenses);
     const isDark = useSelector(state => state.theme.isDark);
+    const downloadRef = useRef(null);
     
     function handleActivatePremium(){
         setPremium(true);
@@ -17,13 +19,27 @@ const Premium = () => {
     function handleThemeChange(){
         dispatch(themeActions.toggleTheme());
     }
+    function handleDownloadFile(){
+        const file = expenses.map((expense) => {
+            let exp = 'amount,category,description\n';
+            for(let id in expense){
+                if(id === 'id')
+                    continue;
+                exp = exp + expense[id] + ",";
+            }
+            return exp;
+        }).join('\n');
+        const blob = new Blob([file]);
+        downloadRef.current.href = URL.createObjectURL(blob);
+        console.log(file);
+    }
     return(
         <div className={classes.main}>
             {!isPremium && expenseAmount > 10000 && <button onClick={handleActivatePremium} className={classes.button}>Activate Premium</button>}
             {isPremium && <div className={classes.features}>
                 {isDark && <div onClick={handleThemeChange} className={classes.dark}><MdDarkMode/></div>}
                 {!isDark && <div onClick={handleThemeChange} className={classes.light}><BsFillBrightnessHighFill/></div>}
-                {<div className={classes.pdf}><MdPictureAsPdf/></div>}
+                {<a ref={downloadRef} onClick={handleDownloadFile} download={"expenses.csv"}><div className={classes.pdf}><MdPictureAsPdf/></div></a>}
             </div>}
         </div>
     )
